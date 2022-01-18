a744aba75a4db79a3c5872366cb3d3822bcaf661
valeria DUBINA <valeriadubina0304@gmail.com>
2022-01-18 20:49:21 +0200

    Implemented the Logger into Service Controller

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -2,15 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Service;
+use App\Services\OtherModelLogger;
 use Illuminate\Http\Request;
 
 class ServicesController extends Controller
 {
-    public function index() {
-        return view('ServiciiPage.serviciiPage');
-    }
+    // public function index() {
 
-    public function show_product($id) {
-        return $id;
+    //     $services = Service::all();
+
+    //     return view('ServiciiPage.serviciiPage', compact('services'));
+    // }
+
+    public function show_product($id, Request $request ,OtherModelLogger $logger) {
+
+        $allServices =  Service::all();
+
+        $service = Service::where('id', $id)->first();
+
+        $logger->mylog($request->user(), $service);
+
+        return view('ServiciiPage.serviciiPage', ['allServices' => $allServices, 
+                                                  'service' => $service]                                          
+        );
     }
 }
