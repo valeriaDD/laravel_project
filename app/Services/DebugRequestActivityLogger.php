a2d1fcee20810ed82816d3af93129d432d4b7411
valeria DUBINA <valeriadubina0304@gmail.com>
2022-01-24 10:41:59 +0200

    Changed DebugLooger info

--- app/Services/DebugRequestActivityLogger.php
+++ app/Services/DebugRequestActivityLogger.php
@@ -3,15 +3,21 @@
 namespace App\Services;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class DebugRequestActivityLogger extends AbstractRequestActivityLogger{
     
     protected function collectRequestData(Request $request): array
     {
         return [
-            'method' => $request->getMethod(),              // Method used in request 
-            'uri' => $request->getRequestUri(),             // Where the request was initiated 
-            'seconds' => microtime(true) - LARAVEL_START,   // Total execution time
+            'date-time' => Carbon::now(), 
+            'method' => $request->getMethod(),                       // Method used in request 
+            'uri' => $request->getRequestUri(),                      // Where the request was initiated 
+            'executionTime' => microtime(true) - LARAVEL_START,      // Total execution time
+            'email' => $request->email,
+            'name' => $request->name,
+            'gender' => $request->gender,                            // Request info
+            'messageText' => $request->messageText,
         ];
     }
 
